45018e7cf63cfa20a674d8f96bffc1f83bce661e
yehyeon son <dara0615@ewhain.net>
2023-05-05 18:59:06 +0900

    기본과제 theme 적용 전

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import App from './App.jsx'
 import './index.css'
 
-
+const theme = {
+  backgroundColor: '#9d9d9d',
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
