07e1c03aa31e993718b9719ece78d9fd291e0142
yehyeon son <dara0615@ewhain.net>
2023-05-11 19:28:33 +0900

    init: 폴더 구조 생성

---
+++ week4_weather/src/Router.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+import Day from './pages/Day';
+import Week from './pages/Week';
+import Daycard from './components/Daycard';
+import Weekcard from './components/Weekcard';
+
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/day" element={<Day/>}>
+                <Route path=":area" element={<Daycard/>} />
+            </Route>
+            <Route path="/week" element={<Week/>}>
+                <Route path=":area" element={<Weekcard/>} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+  )
+}
