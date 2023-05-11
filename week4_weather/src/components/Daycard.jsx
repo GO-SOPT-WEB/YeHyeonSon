197e55fd53538678306167c3b9ceaaf84ccbb4a8
yehyeon son <dara0615@ewhain.net>
2023-05-11 19:47:52 +0900

    feat: Daycard 컴포넌트 스타일링 레이아웃 잡기

---
+++ week4_weather/src/components/Daycard.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Daycard = () => {
+  return (
+    <St.CardWrapper>
+    <St.H1>도시명</St.H1>
+    <St.Text>
+    <p>온도</p>
+    </St.Text>
+    <St.Text>
+    <p>체감 온도</p>
+    </St.Text>
+    <St.Text>
+    <p>최저/최고</p>
+    </St.Text>
+    <St.Text>
+    <p>구름</p>
+    </St.Text>
+    </St.CardWrapper>
+  )
+}
+
+export default Daycard
+
+const St = {
+
+  CardWrapper: styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 4rem;
+  width: 30rem;
+  height: 40rem;
+
+  
+ 
+  background-color: ${({ theme }) => theme.colors.BackgroundBlue};
+  border-radius: 0.5rem;
+  box-shadow: 0 0 10px rgba(142, 157, 244, 0.2);
+`,
+
+ H1: styled.h1`
+  ${({ theme }) => theme.fonts.Content};
+  color: ${({ theme }) => theme.colors.Blue};
+  margin-bottom: 2rem;
+`,
+
+Text: styled.div`
+  ${({ theme }) => theme.fonts.Content};
+  color: ${({ theme }) => theme.colors.Black};
+  margin-bottom: 2rem;
+`,
+
+}
