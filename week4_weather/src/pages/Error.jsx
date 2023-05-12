96f4051fdf18404f487df30418e0ba7fb78c0dfd
yehyeon son <dara0615@ewhain.net>
2023-05-12 18:05:36 +0900

    add: Error컴포넌트 구현, 스타일링

--- week4_weather/src/pages/Error.jsx
+++ week4_weather/src/pages/Error.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Error = () => {
   return (
-    <div>cant find page</div>
+    <ErrorMessage> Error 404 : 찾을 수 없는 페이지입니다 </ErrorMessage>
   )
 }
 
 export default Error
+
+const ErrorMessage = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100vh;
+    
+    font-size: 2rem;
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.Blue};
+`
+
